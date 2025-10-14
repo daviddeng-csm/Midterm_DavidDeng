a70ad6e5f13b3d1a5f4b7f6119fe0187ccd37909
David Deng <daviddeng@my.smccd.edu>
2025-10-14 13:58:36 -0700

    Initial Commit

---
+++ Midterm_DavidDeng/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Midterm_DavidDeng
+//
+//  Created by David Deng on 10/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Midterm_DavidDeng/Midterm_DavidDengApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midterm_DavidDengApp.swift
+//  Midterm_DavidDeng
+//
+//  Created by David Deng on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Midterm_DavidDengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
