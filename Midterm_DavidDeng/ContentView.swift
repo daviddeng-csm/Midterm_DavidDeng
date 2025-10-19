0cd7d725a0eeab2ae47a09aac3a0e4554ef04202
David Deng <daviddeng@my.smccd.edu>
2025-10-19 08:31:34 -0700

    Added DetailView to navigation.

--- Midterm_DavidDeng/ContentView.swift
+++ Midterm_DavidDeng/ContentView.swift
@@ -3,6 +3,8 @@
 //  Midterm_DavidDeng
 //
 //  Midterm Programming Assignment
+//  David Deng - 10/14/25
+//  Retro Console Collection App
 //
 //  Created by David Deng on 10/14/25.
 //
@@ -16,23 +18,78 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(subjects) { subject in
-                HStack {
-                    // Console icon images
-                    Image(subject.imageName + "-icon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 60, height: 60)
-                        .cornerRadius(8)
-                    
-                    // Console name
-                    Text(subject.name)
-                        .font(.headline)
-                        .padding(.leading, 8)
+                NavigationLink(destination: DetailView(subject: subject)) {
+                    HStack {
+                        // Console icon images
+                        Image(subject.imageName + "-icon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                            .cornerRadius(8)
+                        
+                        // Console name
+                        Text(subject.name)
+                            .font(.headline)
+                            .padding(.leading, 8)
+                    }
+                    .padding(.vertical, 8)
                 }
-                .padding(.vertical, 8)
             }
             .navigationTitle("David's Retro Consoles")
         }
+    }
+}
+
+struct DetailView: View {
+    let subject: Subject
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                // Large console image
+                Image(subject.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 220)
+                    .cornerRadius(12)
+                
+                // Console name
+                Text(subject.name)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                // Console details
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Release: \(subject.releaseDate)")
+                    Text("Manufacturer: \(subject.manufacturer)")
+                    Text("Type: \(subject.type.capitalized)")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                // Description
+                Text(subject.description)
+                    .font(.body)
+                    .lineSpacing(4)
+                
+                Divider()
+                
+                // Notable games
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Notable Games:")
+                        .font(.headline)
+                    Text(subject.notableGames)
+                        .font(.body)
+                        .foregroundColor(.secondary)
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
