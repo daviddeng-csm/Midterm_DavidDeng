c1abbbcc490cbee8971e4f6a01b0dd5052c22f44
David Deng <daviddeng@my.smccd.edu>
2025-10-19 00:26:49 -0700

    Updated json file and update Subject file.

--- Midterm_DavidDeng/Subject.swift
+++ Midterm_DavidDeng/Subject.swift
@@ -1,16 +1,20 @@
 //
-//  Dog.swift
-//  DogBreeds
+//  Subject.swift
+//
+//  Standard class to describe item in json. 
 //
 //  Created by Pacheco, Hellen on 10/9/21.
 //
 
 import SwiftUI
 
-struct Subject : Codable, Identifiable {
+struct Subject: Codable, Identifiable {
     var id: String
     var name: String
+    var releaseDate: String
+    var manufacturer: String
+    var type: String
     var description: String
+    var notableGames: String
     var imageName: String
 }
-
