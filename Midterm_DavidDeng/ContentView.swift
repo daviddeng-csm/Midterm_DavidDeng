ae72091ddd8396d4ef7ede307cab07e7ecf7bd05
David Deng <daviddeng@my.smccd.edu>
2025-10-19 00:27:54 -0700

    Added navigation view with a list of consoles.

--- Midterm_DavidDeng/ContentView.swift
+++ Midterm_DavidDeng/ContentView.swift
@@ -10,14 +10,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Load data from JSON file using the Helper.swift extension
+    let subjects: [Subject] = Bundle.main.decode([Subject].self, from: "Data.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(subjects) { subject in
+                HStack {
+                    // Console icon images
+                    Image(subject.imageName + "-icon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 60, height: 60)
+                        .cornerRadius(8)
+                    
+                    // Console name
+                    Text(subject.name)
+                        .font(.headline)
+                        .padding(.leading, 8)
+                }
+                .padding(.vertical, 8)
+            }
+            .navigationTitle("David's Retro Consoles")
         }
-        .padding()
     }
 }
 
